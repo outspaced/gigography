5c88d628afb04aa928ba91a021a603a3508f2c83
Alex Brims <alex.brims@gmail.com>
2016-03-12 22:37:32 +0000

    HTTPS!

--- songkick.sh
+++ songkick.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in `seq 0 10`;
 do
-	curl -G -d "page=$i&apikey=$SONGKICK_API_KEY" http://api.songkick.com/api/3.0/users/outspaced/gigography.xml > $i.xml
+	curl -G -d "page=$i&apikey=$SONGKICK_API_KEY" https://api.songkick.com/api/3.0/users/outspaced/gigography.xml > $i.xml
 done    
